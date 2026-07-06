82322771787cd7f1556b858e96efa39706aeeadd
sophie8909 <sophielien09@gmail.com>
2026-07-06 13:39:52 +0800

    fix: page

--- src/ui/view.js
+++ src/ui/view.js
@@ -182,7 +182,7 @@ export function renderPrimordialStarCumulative(container) {
 }
 
 export function renderTargetLevels(container) {
-  container.className = 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4';
+  container.className = 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4';
   container.innerHTML = '';
 
   targetLevelConfig.forEach((target) => {
